3d7d25530d02a66c1ea19e44286e9b177cef53b0
david-wirelab <info@wirelab.co.uk>
2017-11-28 18:10:23 +0000

    point to ops

---
+++ ops.tf
@@ -0,0 +1,12 @@
+module "ops" {
+  source = "github.com/ukhomeoffice/dq-tf-ops?ref=initial-ops"
+
+  providers = {
+    aws = "aws.APPS"
+  }
+
+  cidr_block            = "10.0.0.0/16"
+  vpc_subnet_cidr_block = "10.0.1.0/24"
+  az                    = "eu-west-2a"
+  name_prefix           = "dq-"
+}
--- terraform.tf
+++ terraform.tf
@@ -6,32 +6,36 @@ variable "CI_ID" {}
 variable "CI_KEY" {}
 
 provider "aws" {
-  region = "eu-west-2"
+  skip_credentials_validation = true
+  region                      = "eu-west-2"
 }
 
 provider "aws" {
-  alias      = "CI"
-  region     = "eu-west-2"
-  access_key = "${var.CI_ID}"
-  secret_key = "${var.CI_KEY}"
+  alias                       = "CI"
+  region                      = "eu-west-2"
+  skip_credentials_validation = true
+  access_key                  = "${var.CI_ID}"
+  secret_key                  = "${var.CI_KEY}"
 }
 
 variable "MOCK_ID" {}
 variable "MOCK_KEY" {}
 
 provider "aws" {
-  alias      = "MOCK"
-  region     = "eu-west-2"
-  access_key = "${var.MOCK_ID}"
-  secret_key = "${var.MOCK_KEY}"
+  alias                       = "MOCK"
+  region                      = "eu-west-2"
+  skip_credentials_validation = true
+  access_key                  = "${var.MOCK_ID}"
+  secret_key                  = "${var.MOCK_KEY}"
 }
 
 variable "APPS_ID" {}
 variable "APPS_KEY" {}
 
 provider "aws" {
-  alias      = "APPS"
-  region     = "eu-west-2"
-  access_key = "${var.APPS_ID}"
-  secret_key = "${var.APPS_KEY}"
+  alias                       = "APPS"
+  region                      = "eu-west-2"
+  skip_credentials_validation = true
+  access_key                  = "${var.APPS_ID}"
+  secret_key                  = "${var.APPS_KEY}"
 }
