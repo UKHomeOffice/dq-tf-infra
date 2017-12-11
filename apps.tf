1d0644528143ee99375e861b7ce60c2a78e1c9da
david-wirelab <info@wirelab.co.uk>
2017-12-11 17:04:05 +0000

    fmt apps

--- apps.tf
+++ apps.tf
@@ -14,6 +14,7 @@ module "apps" {
 output "appsvpc_id" {
   value = "${module.apps.appsvpc_id}"
 }
+
 output "appsvpc_cidr_block" {
-  value  = "${module.apps.appsvpc_cidr_block}"
+  value = "${module.apps.appsvpc_cidr_block}"
 }
