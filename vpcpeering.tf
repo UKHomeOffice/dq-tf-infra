b6d6513c44b01396bb4d3105df4bb2b5c42a1e0b
david-wirelab <info@wirelab.co.uk>
2017-12-11 16:16:10 +0000

    change provider

--- vpcpeering.tf
+++ vpcpeering.tf
@@ -1,17 +1,9 @@
-data "aws_caller_identity" "source" {
-  provider = "aws.APPS"
-}
-
-data "aws_caller_identity" "dest" {
-  provider = "aws.MOCK"
-}
-
 module "peering_to_apps" {
   source = "github.com/UKHomeOffice/tf-peering"
 
   providers = {
-    source = "aws.source"
-    dest   = "aws.source"
+    source = "aws.APPS"
+    dest   = "aws.APPS"
   }
 
   vpc_source_vpc_id = "${module.peering.peeringvpc_id}"
