ea7b0641628888543543f6a28196def52469bdcd
david-wirelab <info@wirelab.co.uk>
2017-12-12 09:32:59 +0000

    change CIDR range

--- ops.tf
+++ ops.tf
@@ -6,7 +6,7 @@ module "ops" {
   }
 
   cidr_block            = "10.2.0.0/16"
-  vpc_subnet_cidr_block = "10.2.1.0/24"
+  vpc_subnet_cidr_block = "10.2.0.0/24"
   az                    = "eu-west-2a"
   name_prefix           = "dq-"
 }
