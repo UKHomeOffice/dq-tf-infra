fdfb904b9fe923e882e95cac8724e8d73f86e5b9
david-wirelab <info@wirelab.co.uk>
2017-12-12 19:23:12 +0000

    full peering config

--- vpcpeering.tf
+++ vpcpeering.tf
@@ -8,4 +8,68 @@ resource "aws_vpc_peering_connection" "peering_to_apps" {
   peer_vpc_id   = "${module.apps.appsvpc_id}"
   peer_owner_id = "${data.aws_caller_identity.apps.account_id}"
   auto_accept   = true
+
+  tags {
+    Name = "Peering and Apps"
+  }
+}
+
+resource "aws_vpc_peering_connection" "peering_to_ops" {
+  provider      = "aws.APPS"
+  vpc_id        = "${module.peering.peeringvpc_id}"
+  peer_vpc_id   = "${module.ops.opsvpc_id}"
+  peer_owner_id = "${data.aws_caller_identity.apps.account_id}"
+  auto_accept   = true
+
+  tags {
+    Name = "Peering and Ops"
+  }
+}
+
+module "peering_to_acpprod" {
+  source = "github.com/UKHomeOffice/tf-peering"
+
+  providers = {
+    aws.source = "aws.APPS"
+    aws.dest   = "aws.MOCK"
+  }
+
+  vpc_source_vpc_id = "${module.peering.peeringvpc_id}"
+  vpc_dest_vpc_id   = "${module.mock-acp.acpprodvpc_id}"
+}
+
+module "peering_to_acpcicd" {
+  source = "github.com/UKHomeOffice/tf-peering"
+
+  providers = {
+    aws.source = "aws.APPS"
+    aws.dest   = "aws.MOCK"
+  }
+
+  vpc_source_vpc_id = "${module.peering.peeringvpc_id}"
+  vpc_dest_vpc_id   = "${module.mock-acp.acpcicdvpc_id}"
+}
+
+module "peering_to_acpops" {
+  source = "github.com/UKHomeOffice/tf-peering"
+
+  providers = {
+    aws.source = "aws.APPS"
+    aws.dest   = "aws.MOCK"
+  }
+
+  vpc_source_vpc_id = "${module.peering.peeringvpc_id}"
+  vpc_dest_vpc_id   = "${module.mock-acp.acpopsvpc_id}"
+}
+
+module "peering_to_acpvpn" {
+  source = "github.com/UKHomeOffice/tf-peering"
+
+  providers = {
+    aws.source = "aws.APPS"
+    aws.dest   = "aws.MOCK"
+  }
+
+  vpc_source_vpc_id = "${module.peering.peeringvpc_id}"
+  vpc_dest_vpc_id   = "${module.mock-acp.acpvpnvpc_id}"
 }
