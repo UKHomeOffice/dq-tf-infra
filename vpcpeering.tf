300f03070b07b2e2c8df422880eecede1310d62e
david-wirelab <info@wirelab.co.uk>
2017-12-12 13:25:21 +0000

    change provider

--- vpcpeering.tf
+++ vpcpeering.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc_peering_connection" "peering_to_apps" {
-  provider      = "aws.source"
+  provider      = "aws.APPS"
   vpc_id        = "${module.peering.peeringvpc_id}"
   peer_vpc_id   = "${module.apps.appsvpc_id}"
   peer_owner_id = "${data.aws_caller_identity.source.account_id}"
