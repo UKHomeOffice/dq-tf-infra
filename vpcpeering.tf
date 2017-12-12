863fd6a0faa0352305ddf1049e54561e02af2907
david-wirelab <info@wirelab.co.uk>
2017-12-12 13:30:37 +0000

    data provider

--- vpcpeering.tf
+++ vpcpeering.tf
@@ -1,7 +1,11 @@
+data "aws_caller_identity" "apps" {
+  provider = "aws.APPS"
+}
+
 resource "aws_vpc_peering_connection" "peering_to_apps" {
   provider      = "aws.APPS"
   vpc_id        = "${module.peering.peeringvpc_id}"
   peer_vpc_id   = "${module.apps.appsvpc_id}"
-  peer_owner_id = "${data.aws_caller_identity.source.account_id}"
+  peer_owner_id = "${data.aws_caller_identity.apps.account_id}"
   auto_accept   = true
 }
