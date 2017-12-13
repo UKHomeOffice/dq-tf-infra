d5463f25f0a96ef6ffe1f773ca8ece933800a19b
nick-otter <nicholas.a.otter@gmail.com>
2017-12-13 10:46:24 +0000

    added route table configuration to ops

--- ops.tf
+++ ops.tf
@@ -1,14 +1,25 @@
 module "ops" {
-  source = "github.com/ukhomeoffice/dq-tf-ops"
+  source = "github.com/UKHomeOffice/dq-tf-ops"
 
   providers = {
     aws = "aws.APPS"
   }
 
-  cidr_block            = "10.2.0.0/16"
-  vpc_subnet_cidr_block = "10.2.0.0/24"
-  az                    = "eu-west-2a"
-  name_prefix           = "dq-"
+  cidr_block                = "10.2.0.0/16"
+  vpc_subnet_cidr_block     = "10.2.0.0/24"
+  az                        = "eu-west-2a"
+  name_prefix               = "dq-"
+  vpc_peering_to_peering_id = "1234"
+  peering_to_acpvpn_id      = "1234"
+
+  route_table_cidr_blocks = {
+    peering_cidr = "${module.peering.peeringvpc_cidr_block}"
+    apps_cidr    = "${module.apps.appsvpc_cidr_block}"
+    acp_vpn      = "${module.mock-acp.acpvpn_cidr_block}"
+    acp_prod     = "${module.mock-acp.acpprod_cidr_block}"
+    acp_ops      = "${module.mock-acp.acpops_cidr_block}"
+    acp_cicd     = "${module.mock-acp.acpcicd_cidr_block}"
+  }
 }
 
 output "opsvpc_id" {
