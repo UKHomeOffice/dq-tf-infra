5852e5c12e80bb0f61343a3834ad24e1053f5987
david-wirelab <info@wirelab.co.uk>
2017-12-13 10:37:07 +0000

    add rt cidrs as a map

--- apps.tf
+++ apps.tf
@@ -1,14 +1,24 @@
 module "apps" {
-  source = "github.com/ukhomeoffice/dq-tf-apps"
+  source = "github.com/UKHomeOffice/dq-tf-apps"
 
   providers = {
     aws = "aws.APPS"
   }
 
-  cidr_block               = "10.1.0.0/16"
-  public_subnet_cidr_block = "10.1.0.0/24"
-  az                       = "eu-west-2a"
-  name_prefix              = "dq-"
+  cidr_block                = "10.1.0.0/16"
+  public_subnet_cidr_block  = "10.1.0.0/24"
+  az                        = "eu-west-2a"
+  name_prefix               = "dq-"
+  vpc_peering_to_peering_id = "${aws_vpc_peering_connection.peering_to_apps.id}"
+
+  route_table_cidr_blocks = {
+    peering_cidr = "${module.peering.peeringvpc_cidr_block}"
+    ops_cidr     = "${module.ops.opsvpc_cidr_block}"
+    acp_vpn      = "${module.mock-acp.acpvpn_cidr_block}"
+    acp_prod     = "${module.mock-acp.acpprod_cidr_block}"
+    acp_ops      = "${module.mock-acp.acpops_cidr_block}"
+    acp_cicd     = "${module.mock-acp.acpcicd_cidr_block}"
+  }
 }
 
 output "appsvpc_id" {
