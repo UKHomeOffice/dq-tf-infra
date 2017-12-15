912c0b6d2b33782f6281106bcccdd22556f0b2d3
david-wirelab <info@wirelab.co.uk>
2017-12-15 10:49:49 +0000

    add private ip and vpc id

--- mock-acp.tf
+++ mock-acp.tf
@@ -5,16 +5,23 @@ module "mock-acp" {
     aws = "aws.MOCK"
   }
 
-  acpcicd_cidr_block            = "10.7.0.0/16"
-  acpcicd_vpc_subnet_cidr_block = "10.7.1.0/24"
-  acpops_cidr_block             = "10.6.0.0/16"
-  acpops_vpc_subnet_cidr_block  = "10.6.1.0/24"
-  acpprod_cidr_block            = "10.5.0.0/16"
-  acpprod_vpc_subnet_cidr_block = "10.5.1.0/24"
   acpvpn_cidr_block             = "10.4.0.0/16"
   acpvpn_vpc_subnet_cidr_block  = "10.4.1.0/24"
+  acpprod_cidr_block            = "10.5.0.0/16"
+  acpprod_vpc_subnet_cidr_block = "10.5.1.0/24"
+  acpops_cidr_block             = "10.6.0.0/16"
+  acpops_vpc_subnet_cidr_block  = "10.6.1.0/24"
+  acpcicd_cidr_block            = "10.7.0.0/16"
+  acpcicd_vpc_subnet_cidr_block = "10.7.1.0/24"
   az                            = "eu-west-2a"
   name_prefix                   = "dq-"
+
+  acp_private_ips = {
+    vpn_tester_ip  = "10.4.1.10"
+    prod_tester_ip = "10.5.1.10"
+    ops_tester_ip  = "10.6.1.10"
+    cicd_tester_ip = "10.7.1.10"
+  }
 
   route_table_cidr_blocks = {
     peering_cidr = "${module.peering.peeringvpc_cidr_block}"
@@ -26,6 +33,7 @@ module "mock-acp" {
     peering_and_acpprod = "${module.peering_to_acpprod.peering_id}"
     peering_and_acpops  = "${module.peering_to_acpops.peering_id}"
     peering_and_acpcicd = "${module.peering_to_acpcicd.peering_id}"
+    ops_and_acpvpn      = "${module.ops_to_acpvpn.peering_id}"
   }
 }
 
