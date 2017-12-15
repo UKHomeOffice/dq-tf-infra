de196c76cdbfe8f81d6ef18fdfdcb559a6a83bda
Chris Nesbitt-Smith <chris@cns.me.uk>
2017-12-15 12:04:00 +0000

    formatting

--- ad.tf
+++ ad.tf
@@ -9,19 +9,19 @@ module "ad" {
     "${module.ops.opsvpc_id}",
     "${module.apps.appsvpc_id}",
   ]
-  cidr_block = "10.99.0.0/16"
-  allow_remote_vpc_dns_resolution = false
-  peer_count = 2
 
-  subnets = [
-//        "${module.apps.appssubnet_cidr_block}",
-//    "10.2.0.0/24",
-//
-//    "10.1.10.0/24",
-  ]
+  cidr_block                      = "10.99.0.0/16"
+  allow_remote_vpc_dns_resolution = false
+  peer_count                      = 2
+
+  subnets = []
+
+  //        "${module.apps.appssubnet_cidr_block}",
+  //    "10.2.0.0/24",
+  //
+  //    "10.1.10.0/24",
 
   subnet_count = 0
-
   Domain = {
     address     = "dq.homeoffice.gov.uk"
     directoryOU = "OU=dqhomeoffice,DC=dqhomeoffice,DC=gov.uk"
