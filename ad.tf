e0ff2eab0ed7292b451f57588d24bd016d00c63c
Chris Nesbitt-Smith <chris@cns.me.uk>
2017-12-15 12:03:38 +0000

    add the ad

--- ad.tf
+++ ad.tf
@@ -5,19 +5,19 @@ module "ad" {
 
   source = "github.com/ukhomeoffice/dq-tf-ad"
 
-  //  source       = "../dq-tf-ad"
   peer_with = [
     "${module.ops.opsvpc_id}",
     "${module.apps.appsvpc_id}",
   ]
-
+  cidr_block = "10.99.0.0/16"
+  allow_remote_vpc_dns_resolution = false
   peer_count = 2
 
   subnets = [
-    //    "${module.apps.appssubnet_cidr_block}",
-    "10.2.0.0/16",
-
-    "10.1.10.0/24",
+//        "${module.apps.appssubnet_cidr_block}",
+//    "10.2.0.0/24",
+//
+//    "10.1.10.0/24",
   ]
 
   subnet_count = 0
@@ -26,4 +26,8 @@ module "ad" {
     address     = "dq.homeoffice.gov.uk"
     directoryOU = "OU=dqhomeoffice,DC=dqhomeoffice,DC=gov.uk"
   }
+}
+
+output "AD Admin Password" {
+  value = "${module.ad.AdminPassword}"
 }
