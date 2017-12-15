7bfcedbffdcaab32a6665f0b83540fb0dfa01101
Chris Nesbitt-Smith <chris@cns.me.uk>
2017-12-15 08:32:01 +0000

    add the active directory

--- ad.tf
+++ ad.tf
@@ -2,20 +2,27 @@ module "ad" {
   providers = {
     aws = "aws.APPS"
   }
-  source       = "github.com/ukhomeoffice/dq-tf-ad"
-//  source       = "../dq-tf-ad"
-  peer_with    = [
+
+  source = "github.com/ukhomeoffice/dq-tf-ad"
+
+  //  source       = "../dq-tf-ad"
+  peer_with = [
     "${module.ops.opsvpc_id}",
     "${module.apps.appsvpc_id}",
   ]
-  peer_count   = 2
-  subnets      = [
-//    "${module.apps.appssubnet_cidr_block}",
+
+  peer_count = 2
+
+  subnets = [
+    //    "${module.apps.appssubnet_cidr_block}",
     "10.2.0.0/16",
+
     "10.1.10.0/24",
   ]
+
   subnet_count = 0
-  Domain       = {
+
+  Domain = {
     address     = "dq.homeoffice.gov.uk"
     directoryOU = "OU=dqhomeoffice,DC=dqhomeoffice,DC=gov.uk"
   }
