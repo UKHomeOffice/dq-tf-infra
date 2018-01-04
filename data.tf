cd29f42ae27a5877a05b3b2d5e88961439a21539
david-wirelab <info@wirelab.co.uk>
2018-01-04 11:49:50 +0000

    kms policy at infra

--- data.tf
+++ data.tf
@@ -13,3 +13,15 @@ data "aws_vpc_peering_connection" "ad_peering" {
   peer_vpc_id   = "${module.ad.vpc_id}"
   peer_owner_id = "${data.aws_caller_identity.apps.account_id}"
 }
+
+data "aws_caller_identity" "apps" {
+  provider = "aws.APPS"
+}
+
+data "aws_caller_identity" "mocks" {
+  provider = "aws.MOCK"
+}
+
+data "aws_caller_identity" "ci" {
+  provider = "aws.CI"
+}
