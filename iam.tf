2f3c775731ddc86bcd0abd9e9ac18a6cf793b9a9
david-wirelab <info@wirelab.co.uk>
2018-02-06 09:34:11 +0000

    add IAM policy

--- iam.tf
+++ iam.tf
@@ -27,6 +27,31 @@ resource "aws_iam_role_policy" "write_to_cw" {
         "Effect": "Allow",
         "Action": "ec2:DescribeTags",
         "Resource": "*"
+    },
+    {
+      "Sid": "DomainJoinPolicy",
+      "Effect": "Allow",
+      "Action": [
+          "ssm:DescribeAssociation",
+          "ssm:ListAssociations",
+          "ssm:GetDocument",
+          "ssm:ListInstanceAssociations",
+          "ssm:UpdateAssociationStatus",
+          "ssm:UpdateInstanceAssociationStatus",
+          "ssm:UpdateInstanceInformation",
+          "ec2messages:AcknowledgeMessage",
+          "ec2messages:DeleteMessage",
+          "ec2messages:FailMessage",
+          "ec2messages:GetEndpoint",
+          "ec2messages:GetMessages",
+          "ec2messages:SendReply",
+          "ds:CreateComputer",
+          "ds:DescribeDirectories",
+          "ec2:DescribeInstanceStatus"
+      ],
+      "Resource": [
+          "*"
+      ]
     }
   ]
 }
