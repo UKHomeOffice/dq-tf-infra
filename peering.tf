73f5832842397d2396757a94a43742c623bdcc1a
david-wirelab <info@wirelab.co.uk>
2018-03-20 10:21:42 +0000

    remove contester subnet and IP

--- peering.tf
+++ peering.tf
@@ -7,9 +7,7 @@ module "peering" {
 
   cidr_block                            = "10.3.0.0/16"
   haproxy_subnet_cidr_block             = "10.3.0.0/24"
-  connectivity_tester_subnet_cidr_block = "10.3.2.0/24"
   public_subnet_cidr_block              = "10.3.3.0/24"
-  peering_connectivity_tester_ip        = "10.3.2.11"
   haproxy_private_ip                    = "10.3.0.11"
   s3_bucket_name                        = "s3-dq-peering-haproxy-config-bucket-${var.NAMESPACE}"
   s3_bucket_acl                         = "private"
