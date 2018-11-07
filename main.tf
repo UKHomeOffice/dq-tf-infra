a2924202a87ed982b44ee28697b48fc4998db144
Duncan Mountford <duncan9562@gmail.com>
2018-11-07 15:15:26 +0000

    Add secrets for Mock and CI accounts

--- main.tf
+++ main.tf
@@ -11,19 +11,19 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-# #provider "aws" {
-#   alias      = "CI"
-#   region     = "eu-west-2"
-#   access_key = "${var.CI_ID}"
-#   secret_key = "${var.CI_KEY}"
-# }
-#
-# provider "aws" {
-#   alias      = "MOCK"
-#   region     = "eu-west-2"
-#   access_key = "${var.MOCK_ID}"
-#   secret_key = "${var.MOCK_KEY}"
-# }
+provider "aws" {
+  alias      = "CI"
+  region     = "eu-west-2"
+  access_key = "${var.CI_ID}"
+  secret_key = "${var.CI_KEY}"
+}
+
+provider "aws" {
+  alias      = "MOCK"
+  region     = "eu-west-2"
+  access_key = "${var.MOCK_ID}"
+  secret_key = "${var.MOCK_KEY}"
+}
 
 provider "aws" {
   alias      = "APPS"
