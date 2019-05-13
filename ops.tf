b70e475941461a77ef3b524f876214874cbb8d39
david-wirelab <info@wirelab.co.uk>
2019-05-13 11:52:36 +0100

    Remove bastion 4 and 5

--- ops.tf
+++ ops.tf
@@ -16,8 +16,6 @@ module "ops" {
   bastion_windows_ip                           = "${var.NAMESPACE == "prod" ? "10.2" : "10.8"}.0.12"
   bastion2_windows_ip                          = "${var.NAMESPACE == "prod" ? "10.2" : "10.8"}.0.13"
   bastion3_windows_ip                          = "${var.NAMESPACE == "prod" ? "10.2" : "10.8"}.0.14"
-  bastion4_windows_ip                          = "${var.NAMESPACE == "prod" ? "10.2" : "10.8"}.0.15"
-  bastion5_windows_ip                          = "${var.NAMESPACE == "prod" ? "10.2" : "10.8"}.0.16"
   management_access                            = "${var.NAMESPACE == "prod" ? "10.2" : "10.8"}.0.11/32"
   analysis_instance_ip                         = "${var.NAMESPACE == "prod" ? "10.2" : "10.8"}.2.8"
   tableau_dev_ip                               = "${var.NAMESPACE == "prod" ? "10.2" : "10.8"}.6.10"
