33fafef560d7b4600e7436ca69691e693c44732c
Srikanth Arunachalam <116078089+s-arunachalam@users.noreply.github.com>
2023-11-15 14:28:42 +0000

    feat: yel-8578 modified to remove default

--- variables.tf
+++ variables.tf
@@ -20,11 +20,6 @@ variable "DOMAIN_JOINER_PWD" {
 
 variable "account_id" {
   type = map(string)
-  default = {
-    "test"    = "797728447925"
-    "notprod" = "483846886818"
-    "prod"    = "337779336338"
-  }
 }
 
 variable "AdminPassword" {
