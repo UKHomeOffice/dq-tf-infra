640ebd2a22a15c4b13ed6b90604322397f01d484
Ian Relf <ian.relf@digital.homeoffice.gov.uk>
2023-12-22 12:30:52 +0000

    Use dynamically created provider and terraform files - as per other repos

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
-terraform {
-  backend "s3" {
-  }
-}
+#terraform {
+#  backend "s3" {
+#  }
+#}
 
 locals {
   naming_suffix  = "${var.NAMESPACE}-dq"
   iam_role_count = length(module.apps.iam_roles)
 }
 
-provider "aws" {
-  region = "eu-west-2"
-}
+#provider "aws" {
+#  region = "eu-west-2"
+#}
 
 provider "aws" {
   alias      = "CI"
@@ -18,13 +18,6 @@ provider "aws" {
   access_key = var.CI_ID
   secret_key = var.CI_KEY
 }
-
-#provider "aws" {
-#  alias      = "APPS"
-#  region     = "eu-west-2"
-#  access_key = var.APPS_ID
-#  secret_key = var.APPS_KEY
-#}
 
 provider "aws" {
   alias      = "ENV_ACCT"
