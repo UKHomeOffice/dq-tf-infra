4361b0c0cc0f9331f40033e84d404f22cc334a97
Ian Relf <ian.relf@digital.homeoffice.gov.uk>
2023-12-22 12:32:12 +0000

    Removed commented out code

--- main.tf
+++ main.tf
@@ -1,16 +1,7 @@
-#terraform {
-#  backend "s3" {
-#  }
-#}
-
 locals {
   naming_suffix  = "${var.NAMESPACE}-dq"
   iam_role_count = length(module.apps.iam_roles)
 }
-
-#provider "aws" {
-#  region = "eu-west-2"
-#}
 
 provider "aws" {
   alias      = "CI"
