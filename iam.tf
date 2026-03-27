ead5028e9e926325be82d10cc9111fd0f78cfc8c
D-Gurukishore Bolisetty <d-boliseg@U-16XTRFGVJW9XA.dev.dacc.phz>
2026-03-27 10:13:55 +0000

    fix: json format correction

--- iam.tf
+++ iam.tf
@@ -120,9 +120,6 @@ resource "aws_iam_policy" "write_to_cw_new" {
           "ds:CreateComputer",
           "ds:DescribeDirectories",
           "ec2:DescribeInstanceStatus",
-          # ─────────────────────────────────────────────────────────────
-          # NEW ssmmessages permissions (required after 16 June 2026)
-          # ─────────────────────────────────────────────────────────────
           "ssmmessages:CreateControlChannel",
           "ssmmessages:CreateDataChannel",
           "ssmmessages:OpenControlChannel",
